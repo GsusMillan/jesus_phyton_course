8027b26bb2c0195be93cd5fc070894fce17e8453
Jesus Outpost QA <jesus.mh17@gmail.com>
2023-02-14 19:37:24 -0700

    Actividad numero 03 (mejorada con inputs)

--- phyton_03.py
+++ phyton_03.py
@@ -1,19 +1,9 @@
-print("¿Cuál es su nombre?")
-nombre = "Jesus"
-print("¿Cuál es su edad?")
-edad = 25
-print("¿En que ciudad vive")
-ciudad = "Mazatlán"
-print("¿Estado civil?")
-estado_civil = bool(False)
-if(estado_civil == False):
-    estado_civil = "soltero"
-else:
-    estado_civil = "casado"
-print(estado_civil)
-print("Ingrese un valor entre 1 y 100")
-primer_valor = 20
-print("Ingrese un valor entre 100 y 200")
-segundo_valor = 140
-resultado = (primer_valor / segundo_valor)
-print(f"Mi nombre es {nombre}, mi edad es {edad}, soy de la ciudad de {ciudad} y mi resultado es {resultado}")
+nombre = str(input("¿Cuál es su nombre? :"))
+edad = str(input("¿Cuál es su edad? :"))
+ciudad = str(input("¿En que ciudad vive? :"))
+estado_civil = str(input("¿Estado civil? :"))
+valor_a = int(input("Ingrese un número entre 1 - 100 :"))
+valor_b = int(input("Ingrese un número entre 100 - 200 :"))
+resultado = valor_a/valor_b
+print(resultado)
+print(f"Mi nombre es {nombre}, mi edad es {edad} años, soy de la ciudad de {ciudad} y mi resultado es {resultado}")
