90d803d8656f55c917bafd6bf09096c592e36fa3
Jesus Outpost QA <jesus.mh17@gmail.com>
2023-02-16 19:00:48 -0700

    Actividad numero 03 (mejorada con inputs)

---
+++ phyton_05.py
@@ -0,0 +1,74 @@
+"""Entregable ejercicio modulo 5 (python_05.py):
+Pide al usuario dos variables a = 12.5 y b = 34, crea funciones que permitan calcular la suma,
+resta, multiplicación y división, como también el valor del módulo de b entre a
+Crea un método que permita convertir cualquier numero entero y a flotante
+Extra: Define una función para convertir de grados Celsius a Fahrenheit,
+pide al usuario que ingrese la temperatura en Celsius e imprima la conversión."""
+def suma(param1, param2):
+    sum = param1 + param2
+    return sum
+def resta(param1, param2):
+    res = param1 - param2
+    return res
+
+def multiplicacion(param1, param2):
+    mul = param1 * param2
+    return mul
+
+def division(param1, param2):
+    div = param1 / param2
+    return div
+
+def modulo(param1, param2):
+    mod = param1 % param2
+    return mod
+
+def convierte_enteros(param1: int):
+    conv = float(param1)
+    return conv
+
+def conversor_celcius_to_farenheit(param1: int):
+    parametro1 = float(1.8)
+    parametro2 = int(32)
+    con = param1 * parametro1
+    con += 32
+    return con
+
+
+value_1 = int(input("ingresa un numero:"))
+value_2 = int(input("ingresa otro numero:"))
+sum = suma(value_1, value_2)
+print(sum)
+res = resta(value_1, value_2)
+print(res)
+mul = multiplicacion(value_1, value_2)
+print(mul)
+div = division(value_1, value_2)
+print(div)
+mod = modulo(value_1, value_2)
+print(mod)
+
+valor_flotante = int(input("ingresa un numero entero:"))
+result = convierte_enteros(valor_flotante)
+print(result)
+
+celcius = int(input("Ingresa los grados celcius a convertir en Farenheit:"))
+show = conversor_celcius_to_farenheit(celcius)
+print(show)
+
+"""""EXTRA:
+Escribe una función llamada "es_par" que tome un número entero como parámetro y 
+devuelva True si el número es par o False si el número es impar.
+Luego, escribe un programa que pida al usuario que introduzca un número entero y
+utilice la función "es_par" para determinar si el número es par o impar. 
+Imprime en pantalla un mensaje indicando si el número es par o impar."""
+
+def es_par(param1):
+    return param1%2
+
+num = int(input("Ingresa un numero para verificar si es par o inpar"))
+par_inpar = es_par(num)
+if (par_inpar == 0):
+    print("El numero ingresado es Par")
+else:
+    print("El numero ingresado es Inpar")
